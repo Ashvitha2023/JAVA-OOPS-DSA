15340507bf7aecd98ea9fec3db0fbcabba3867d8
Ashvitha2023 <ashvithakesavan2005@gmail.com>
2025-07-21 10:05:57 +0530

    Using tail recursion

---
+++ Math/Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+    static int fact(int a,int re){
+       if(a==1){
+        return re;
+       }
+       return fact(a-1,re*a);
+    }
+    public static void main(String[] args) {
+        int a=5;
+        int c=fact(a,1);
+        System.out.println(c);
+    }
+}
