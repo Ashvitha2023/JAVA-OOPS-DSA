c32a95cd81e93e1a239861540372e03ec740f8b8
Ashvitha2023 <ashvithakesavan2005@gmail.com>
2025-08-14 21:31:15 +0530

    Loose coupling

--- OOPS/HasARelationship.java
+++ OOPS/HasARelationship.java
@@ -1,12 +1,20 @@
 //Has-A (composition) means the class contains another class as a member, not just temporarily inside a method.
 //it is a loose coupling 
 /*Order can use any type of Payment without changing code.*/
-
-class Payment{
+interface Payment{
+    void m2();
+}
+class Paytm implements Payment{
     public void m2(){
-        System.out.println("Payment is succesfull");
+        System.out.println("Payment in paytm is succesfull");
     }
 }
+class Card implements Payment{
+    public void m2(){
+        System.out.println("Payment in card is succesfull");
+    }
+}
+
 class Order{
     Payment p;
     Order(Payment p){
@@ -20,11 +28,11 @@ class Order{
 }
 public class HasARelationship {
     public static void main(String[] args) {
-        Payment Paypal=new Payment();
-        Order a=new Order(Paypal);
+        Paytm p=new Paytm();
+        Order a=new Order(p);
         a.m1();
-        Payment Paytm=new Payment();
-        Order b=new Order(Paytm);
+        Card h=new Card();
+        Order b=new Order(h);
         b.m1();
     }
     
