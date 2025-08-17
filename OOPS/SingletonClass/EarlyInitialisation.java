9dc1e43f9761f7c1d250b5cff30f5dc8b48173ac
Ashvitha2023 <ashvithakesavan2005@gmail.com>
2025-08-17 17:53:27 +0530

    added

---
+++ OOPS/SingletonClass/EarlyInitialisation.java
@@ -0,0 +1,16 @@
+class Test{
+    private static Test instance=new Test();
+    private Test(){
+
+    }
+    public static Test getInstance(){
+        return instance;
+    }
+}
+class EarlyInitialisation{
+    public static void main(String[] args) {
+        Test a=Test.getInstance();
+       Test b=Test.getInstance();
+       System.out.println(a==b);
+    }
+}
