d272c7ce1f197f33ed9a8e3c79511afa68451a7c
Ashvitha2023 <ashvithakesavan2005@gmail.com>
2025-09-01 21:31:36 +0530

    added

---
+++ Identifiers.java
@@ -0,0 +1,22 @@
+package DSA;
+
+public class Identifiers {
+    public static void main(String[] args) {
+    //A-Z and a-z and $_
+    int total@me=10;// W
+    int total_me=10;
+    int total-me=10;// W 
+
+    //It should not start with digits
+    int Java24=10;
+    int 24Java=10;// W
+
+    //Reserved words cannot be used
+    int if=10;// W
+
+    //There is no limit  
+
+    //Class names can be used
+    int Identifiers=10;
+    }
+}
