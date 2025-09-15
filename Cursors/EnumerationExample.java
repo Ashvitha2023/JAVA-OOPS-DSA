1a052d47ec13c1d44f3743b023a8efb18626664c
Ashvitha2023 <ashvithakesavan2005@gmail.com>
2025-09-15 15:42:54 +0530

    added

---
+++ Cursors/EnumerationExample.java
@@ -0,0 +1,19 @@
+//Enumeration is used only in legal classes like Vector,HashTable,Stack
+//Enumeration is only used for retrieval
+
+import java.util.*;
+public class EnumerationExample {
+    public static void main(String[] args) {
+        Vector<Integer> v=new Vector<>();
+        v.add(1);
+        v.add(2);
+        v.add(3);
+        v.add(4);
+        v.add(5);
+        v.add(6);
+        Enumeration <Integer> e=v.elements();
+        while(e.hasMoreElements()){
+            System.out.println(e.nextElement());
+        }
+    }
+}
